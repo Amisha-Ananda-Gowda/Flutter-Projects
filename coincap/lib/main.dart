f187aa2a5234dca1e1e0fa6c2cad42ba4734e118
Amisha <21j03.amisha@sjec.ac.in>
2023-12-02 17:59:32 +0530

    created new app

---
+++ coincap/lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:coincap/models/app_config.dart';
+import 'package:coincap/pages/home_page.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await loadConfig();
+
+  runApp(const MyApp());
+}
+
+Future<void> loadConfig() async {
+  String _configContent =
+      await rootBundle.loadString("assets/config/main.json");
+  Map _configData = jsonDecode(_configContent);
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(
+      COIN_API_BASE_URL: _configData["COIN_API_BASE_URL"],
+    ),
+  );
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CoinCap',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Color.fromARGB(255, 158, 60, 197),
+      ),
+      home: HomePage(),
+    );
+  }
+}
